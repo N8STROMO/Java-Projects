1ee8ccc576a7600eef64ad45d71c0170549d19e6
N8STROMO <johntpaul93@gmail.com>
2017-05-29 16:28:18 -0400

    Edits to Caesar Project

--- Caesar Cipher/src/Caesar.java
+++ Caesar Cipher/src/Caesar.java
@@ -19,6 +19,15 @@ public class Caesar
         while (typeOfCipher != 1 || typeOfCipher != 2)
         {
             //Ask the user if they want to encrypt or decrypt the string
+            System.out.println("Press 1 for Encypt and 2 for Decrypt: ");
+
+            if(typeOfCipher == 1)
+            {
+                //Generate random number by which to shift the alaphet by
+                Random randomNumber = new Random();
+                int shift = randomNumber.nextInt(1, 26);
+            }
+
         }
     }
 }
