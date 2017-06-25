e1197582c28958bdf00da9a9425891bfa29db6ca
N8STROMO <johntpaul93@gmail.com>
2017-06-25 00:35:34 -0400

    Formatted 9.1. Started new pyramid project.

---
+++ Pyramid/src/Pyramid.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class Pyramid {
+  public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a width for the pyramid: ");
+    int pyramidWidth = input.nextInt();
+
+    for (int x = 0; x < pyramidWidth; x++)
+    {
+      for (int y = 0; y < pyramidWidth; y--)
+      {
+
+      }
+    }
+  }
+}
--- Revel/Exercise09_01/src/Exercise09_01.java
+++ Revel/Exercise09_01/src/Exercise09_01.java
@@ -2,11 +2,6 @@
  * Created by johnt on 4/2/2017.
  */
 public class Exercise09_01 {
-    public static void main(String args[])
-    {
-        for (:
-             ) {
-            
-        }
-    }
+  public static void main(String args[]) {
+  }
 }
