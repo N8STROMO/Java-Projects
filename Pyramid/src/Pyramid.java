63c71b6aa637b1055da69c95f03f13f51d8be90d
N8STROMO <johntpaul93@gmail.com>
2017-07-31 11:48:46 -0400

    Finished Pyramid / Started Pyramid Refactor

--- Pyramid/src/Pyramid.java
+++ Pyramid/src/Pyramid.java
@@ -4,33 +4,39 @@ Displays numbers in a pyramid pattern
 import java.util.*;
 public class Pyramid {
   public static void main(String args[]) {
-    int startSpace = 0;
+    int startRight = 0;
     int pyramidWidth;
 
 
     Scanner input = new Scanner(System.in);
     System.out.println("Enter a width for the pyramid: ");
     pyramidWidth = input.nextInt();
-    pyramidWidth = pyramidWidth;
 
-    // TODO How is pyramidWidth being used?
+    // TODO Better way to implement considering pyramid width
 
-    for (int row = startSpace; row < pyramidWidth; row++)
+    for (int row = 1; row < pyramidWidth; row += row)
     {
       // Controls white space
-      for (int j = 0; j < pyramidWidth - row; j++)
+      for (int startSpace = 0; startSpace < pyramidWidth; startSpace++)
       {
-        System.out.print("  ");
+        System.out.print(" ");
       }
 
-      // Displays ascending numners
-      for (int k = 0; k <= row; k++) {
-        System.out.print("1 ");
+      // Displays ascending numbers
+      for (int leftSide = 1; leftSide <= row; leftSide += leftSide)
+      {
+        System.out.print("0");
       }
 
-      // TODO Display descending numbers
+      // Display descending numbers
+      for (int rightSide = startRight; rightSide > 0; rightSide /= 2)
+      {
+        System.out.print("0");
+      }
 
       System.out.println();
+      pyramidWidth--;
+      startRight += row;
     }
   }
 }
---
+++ Pyramid2/src/Pyramid.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Pyramid {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a width for the pyramid: ");
+        int pyramidWidth = input.nextInt();
+
+        for (int row = 1; row < pyramidWidth; row++) {
+            for (int start = 0; start < pyramidWidth; pyramidWidth--) {
+                System.out.print(" ");
+            }
+
+
+        }
+    }
+
+    public void PyramidHeight()
+    {
+
+    }
+
+    }
