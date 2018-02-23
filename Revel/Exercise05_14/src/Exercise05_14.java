5f9f9bc9401d31b697c5e17fb65958fd61d6513d
John T Paul <johntpaul93@gmail.com>
2018-02-23 18:46:52 -0500

    Instructions

---
+++ Revel/Exercise05_14/src/Exercise05_14.java
@@ -0,0 +1,13 @@
+/**
+ * First find d to be the minimum of n1 and n2, then check whether d, d-1, d-2, ..., 2, or 1 is a divisor for both n1
+ * and n2 in this order. The first such common divisor is the greatest common divisor for n1 and n2. Write a program
+ * that prompts the user to enter two positive integers and displays the gcd.
+ */
+
+import java.util.*;
+public class Exercise05_14 {
+
+  public static void main(String[] args) {
+
+  }
+}
