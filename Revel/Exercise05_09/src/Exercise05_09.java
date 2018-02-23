9f062076e482e0cb68eca00af16c0e925064a02b
John T Paul <johntpaul93@gmail.com>
2018-02-23 18:43:07 -0500

    Instructions

--- Revel/Exercise05_09/src/Exercise05_09.java
+++ Revel/Exercise05_09/src/Exercise05_09.java
@@ -1,5 +1,6 @@
 /**
- *
+ * Write a program that prompts the user to enter the number of students and each student’s name and score, and
+ * finally displays the student with the highest score and the student with the second-highest score.
  */
 
 import java.util.*;
